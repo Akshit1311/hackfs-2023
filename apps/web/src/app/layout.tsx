8fdf43190b014b03ae6827a6e511a474130bc40c
anjanaaaaaaaa <thanjana7@gmail.com>
2023-06-11 23:24:05 +0530

    Complete:Saturn browser retrieval

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -8,6 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://saturn.tech/widget.js" />
+      </head>
       <body>
         <Header />
         <main className="bg-base min-h-screen w-full text-white max-w-screen-8xl mx-auto font-inter">
