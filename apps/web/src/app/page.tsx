ec8cffa2425d84fc2b457887b4471e694e054305
harry-sketch <harshbisht90@gmail.com>
2023-06-17 02:09:14 +0530

    add: rainbow kit

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,24 @@
+"use client";
 import React from "react";
+
+// Connect Wallet
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 type pageProps = {};
 
 const page: React.FC<pageProps> = () => {
   return (
-    <div className="text-white">
-      <h1>GumroaD</h1>
+    <div className="text-white flex flex-col items-center justify-center w-full h-screen font-mabry">
+      <h1 className="text-[23rem]">GumroaD</h1>
+      <ConnectButton />
+      <div className="mt-4 flex items-center w-full h-full">
+        <div className="border w-full bg-pink-400 flex items-center justify-center h-full text-6xl  text-black">
+          Go from zero to $
+        </div>
+        <div className="border w-full bg-yellow-400 flex items-center justify-center h-full text-6xl text-black">
+          Get Start Right now!!
+        </div>
+      </div>
     </div>
   );
 };
