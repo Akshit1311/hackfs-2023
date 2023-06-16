4b4a01bbba77da04a73970979d7f60b881284475
harry-sketch <harshbisht90@gmail.com>
2023-06-17 02:21:09 +0530

    add: connect button

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,14 +3,23 @@ import React from "react";
 
 // Connect Wallet
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useRouter } from "next/navigation";
 
 type pageProps = {};
 
 const page: React.FC<pageProps> = () => {
+  const { push } = useRouter();
+
   return (
     <div className="text-white flex flex-col items-center justify-center w-full h-screen font-mabry">
       <h1 className="text-[23rem]">GumroaD</h1>
-      <ConnectButton />
+      <div
+        className="scale-150"
+        role="presenatation"
+        onClick={() => push("/discover")}
+      >
+        <ConnectButton />
+      </div>
       <div className="mt-4 flex items-center w-full h-full">
         <div className="border w-full bg-pink-400 flex items-center justify-center h-full text-6xl  text-black">
           Go from zero to $
